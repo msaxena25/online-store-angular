f61b05df628e55e8545d171d5b4a66d80e09bd9d
Mohit <mohits@nedbank.co.za>
2025-05-06 17:39:00 +0530

    Acknowledgement page added

--- src/app/modules/acknowledgment/components/acknowledgment-page/acknowledgment-page.component.ts
+++ src/app/modules/acknowledgment/components/acknowledgment-page/acknowledgment-page.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { BaseComponent } from '../../../../core/components/base.components';
+import { RouteUrls } from '../../../../core/constants/route.urls.constants';
 
 @Component({
   selector: 'app-acknowledgment-page',
   templateUrl: './acknowledgment-page.component.html',
   styleUrls: ['./acknowledgment-page.component.scss']
 })
-export class AcknowledgmentPageComponent {
+export class AcknowledgmentPageComponent extends BaseComponent {
+  orderId: string = '';
+  orderDate: Date = new Date();
+  constructor() {
+    super();
+  }
 
+  ngOnInit(): void {
+    this.orderId = 'ORD-' + Math.floor(100000 + Math.random() * 900000);
+  }
+
+  goHome(): void {
+    this.navigateTo(RouteUrls.route.productList);
+  }
 }
